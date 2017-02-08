cc9d2ab84d11a7a5928e22bf780f0b9fbc2233ac
Eric Wong <cpterack@gmail.com>
2017-02-07 21:05:12 -0800

    Updated tables
    Added fields for first half of tables.

--- DB/paws_db.sql
+++ DB/paws_db.sql
@@ -28,39 +28,96 @@ from scratch
 
 CREATE TABLE litters ( 
 	id INT AUTO_INCREMENT PRIMARY KEY,
-    litter_name VARCHAR(255) NOT NULL
+	kc_ref_id INT NOT NULL,
+    litter_name VARCHAR(255) NOT NULL,
+    cat_count INT NOT NULL,
+    kitten_count INT NOT NULL,
+    dob DATE,
+    asn_start DATE,						
+    asn_end DATE,
+    est_arrival VARCHAR(50),
+    breed VARCHAR(255),
+    foster_notes VARCHAR(255),
+    notes TEXT,
+    created DATETIME
 );
 
 
 CREATE TABLE adopters ( 
-	id INT AUTO_INCREMENT PRIMARY KEY  
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	phone VARCHAR(10) NOT NULL,
+	cat_count INT NOT NULL,
+	address VARCHAR(255) NOT NULL,
+	notes TEXT,
+	created DATETIME
 ); 
 
 
 CREATE TABLE fosters ( 
-	id INT AUTO_INCREMENT PRIMARY KEY  
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	phone VARCHAR(10) NOT NULL,
+	address VARCHAR(255) NOT NULL,
+	exp VARCHAR(255),
+	pets VARCHAR(255),
+	kids VARCHAR(255),
+	avail VARCHAR(255),
+	rating INT,
+	notes TEXT,
+	created DATETIME
 ); 
 
 
 CREATE TABLE cats ( 
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	litter_id INT,
-	FOREIGN KEY (litter_id) REFERENCES litters(id)
+	adopter_id INT,
+	foster_id INT,
+	name VARCHAR(255) NOT NULL,
+	is_kitten BIT(1) NOT NULL,
+	dob DATE NOT NULL,
+	is_female BIT(1) NOT NULL,
+	breed VARCHAR(255) NOT NULL,
+	bio TEXT,
+	asn DATE,
+	caretaker_notes VARCHAR(255),
+	medical_notes TEXT,					
+	microchip INT,
+	microchip_date DATE,	
+	created DATETIME,
+	FOREIGN KEY litter_ref (litter_id) REFERENCES litters(id),
+	FOREIGN KEY adopter_ref (adopter_id) REFERENCES adopters(id),
+	FOREIGN KEY foster_ref (foster_id) REFERENCES fosters(id)
 ); 
 
 
 CREATE TABLE cathistory ( 
-	id INT AUTO_INCREMENT PRIMARY KEY  
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	cat_id INT NOT NULL,
+	adopter_id INT,
+	foster_id INT,	
+	start_date DATE NOT NULL,
+	end_date DATE,
+	FOREIGN KEY cat_ref (cat_id) REFERENCES cats(id),
+	FOREIGN KEY adopter_ref (adopter_id) REFERENCES adopters(id),
+	FOREIGN KEY foster_ref (foster_id) REFERENCES fosters(id)	
 ); 
 
 
 CREATE TABLE adoptionevents ( 
-	id INT AUTO_INCREMENT PRIMARY KEY  
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	event_date DATE NOT NULL,
+	description VARCHAR(50)
 ); 
 
 
 CREATE TABLE cats_adoptionevents ( 
-	id INT AUTO_INCREMENT PRIMARY KEY  
+	id INT AUTO_INCREMENT PRIMARY KEY, 
+	cat_id INT NOT NULL,
+	event_id INT NOT NULL,
+	FOREIGN KEY cat_ref (cat_id) REFERENCES cats(id),
+	FOREIGN KEY event_ref (event_id) REFERENCES adoptionevents(id)
 ); 
 
 
