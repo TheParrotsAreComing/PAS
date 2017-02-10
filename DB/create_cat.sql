e025edf0dbe4dd0527a7670871e023c5729861ec
austinlxx <heero916@hotmail.com>
2017-02-09 20:32:24 -0800

    Updated create_cat.sql for modified paws_db file

--- DB/create_cat.sql
+++ DB/create_cat.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name, is_kitten, dob, is_female, breed, bio, vaccine_date, created)
-VALUES ("Rogue",1,'2001-03-20',1,"Wolverine","Fast health regeneration, adamantium claws, aggressive...",'2110-01-20',NOW());
-INSERT INTO cats (name, is_kitten, dob, is_female, breed, bio, vaccine_date, created)
-VALUES ("Nightcrawler",1,'2003-09-02',0,"Alien","Teleportation, high agility, above average intelligence",'2004-02-18',NOW());
-INSERT INTO cats (name, is_kitten, dob, is_female, breed, bio, vaccine_date, created)
-VALUES ("Mystique",0,'1929-12-04',1,"Shapeshifter","High metabolic cell structure",'2019-04-10',NOW());
+INSERT INTO cats (name, is_kitten, dob, is_female, breed, bio, created)
+VALUES ("Rogue",1,'2001-03-20',1,"Wolverine","Fast health regeneration, adamantium claws, aggressive...",NOW());
+INSERT INTO cats (name, is_kitten, dob, is_female, breed, bio, created)
+VALUES ("Nightcrawler",1,'2003-09-02',0,"Alien","Teleportation, high agility, above average intelligence",NOW());
+INSERT INTO cats (name, is_kitten, dob, is_female, breed, bio, created)
+VALUES ("Mystique",0,'1929-12-04',1,"Shapeshifter","High metabolic cell structure",NOW());
