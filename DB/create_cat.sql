f458500d65777879e60ec2428509e04f42ce410e
Ron Lewis <ron.lewis@gmail.com>
2017-02-16 11:09:16 -0800

    updated seed scripts added a deleted cat to cats script added a create litters script (includes cats in the litter)

--- DB/create_cat.sql
+++ DB/create_cat.sql
@@ -8,3 +8,6 @@ VALUES ("Nightcrawler",1,'2003-09-02',0,"Alien","Teleportation, high agility, ab
 
 INSERT INTO cats (cat_name, is_kitten, dob, is_female, breed, bio, created,is_deleted)
 VALUES ("Mystique",0,'1929-12-04',1,"Shapeshifter","High metabolic cell structure",NOW(),false);
+
+INSERT INTO cats (cat_name, is_kitten, dob, is_female, breed, bio, created,is_deleted)
+VALUES ("Deleted Cat Test",0,'2000-01-01',0,"DSH Minx/Calico mix","Highly energetic, out to kill people",NOW(),true);
---
+++ DB/create_litters.sql
@@ -0,0 +1,18 @@
+use paws_db;
+
+-- make a litter
+INSERT INTO litters (kc_ref_id, litter_name, cat_count, kitten_count, dob, est_arrival, breed, foster_notes, notes, created, is_deleted)
+VALUES (99999,"A Test Litter",1,3,'2017-02-14',"Early March","Minx/Tabby/Calico mixes","Still needs foster home","Litter that needs to stay together, and needs special care",NOW(),false);
+
+-- add a mom and kittens to it
+INSERT INTO cats (cat_name, is_kitten, dob, is_female, breed, bio, created,is_deleted, litter_id)
+VALUES ("Mary, the Litter Mom",false,'2007-03-20',true,"DSH Minx","Very caring mother of kittens",NOW(),false,
+	(SELECT id FROM litters WHERE litter_name="A Test Litter"));
+
+INSERT INTO cats (cat_name, is_kitten, dob, is_female, breed, bio, created,is_deleted, litter_id)
+VALUES ("Edgar the Kitten",true,'2017-02-01',false,"DLH Minx/Tabby mix","Very playful and outgoing",NOW(),false,
+	(SELECT id FROM litters WHERE litter_name="A Test Litter"));
+
+INSERT INTO cats (cat_name, is_kitten, dob, is_female, breed, bio, created,is_deleted, litter_id)
+VALUES ("Sally the Kitten",true,'2017-02-01',true,"DSH Minx/Calico mix","Very shy (except with bonded mom and brother), but very playful",NOW(),false,
+	(SELECT id FROM litters WHERE litter_name="A Test Litter"));
