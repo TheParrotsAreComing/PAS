94398b1f696e93cd6401b82d33abd80385c6b367
Eric Bollinger <ericsbollinger@gmail.com>
2017-02-23 23:47:15 -0800

    Removed microchip date from form and DB, made cat date of birth a required field in Cats/Add

--- DB/sprint2_deploy.sql
+++ DB/sprint2_deploy.sql
@@ -3,4 +3,4 @@ ALTER TABLE cats MODIFY COLUMN breed VARCHAR(75) NOT NULL;
 ALTER TABLE cats ADD color VARCHAR(75) NOT NULL;
 ALTER TABLE cats ADD coat VARCHAR(75) NOT NULL;
 ALTER TABLE cats CHANGE COLUMN medical_notes specialty_notes TEXT;
-ALTER TABLE cats DROP COLUMN caretaker_notes; 
+ALTER TABLE cats DROP COLUMN caretaker_notes microchip_date;
