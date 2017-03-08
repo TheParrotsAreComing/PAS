94cab648df00838a5a037d01633d830e36671934
Ron Lewis <ron.lewis@gmail.com>
2017-03-07 22:16:08 -0800

    fixed cats.js to load moment.js, instead of having the page load it everywhere its needed

--- webroot/js/cats.js
+++ webroot/js/cats.js
@@ -1,26 +1,28 @@
 function calculateAndPopulateAgeFields() {
-	var cat_dob_divs = $('.cat-dob');
-	var cat_age_divs = $('.cat-age');
+	$.getScript('js/moment.js', function() {
+		var cat_dob_divs = $('.cat-dob');
+		var cat_age_divs = $('.cat-age');
 
-	for(var i = 0; i < cat_dob_divs.length; i++) {
-		// get and parse the DOB field on the page
-		var cat_dob = cat_dob_divs[i].innerHTML;
-		var cat_dob_details = cat_dob.split("/");
-		
-		var month = parseInt(cat_dob_details[0]) - 1;
-		var day = parseInt(cat_dob_details[1]);
-		var year = parseInt(cat_dob_details[2]) + 2000;
+		for(var i = 0; i < cat_dob_divs.length; i++) {
+			// get and parse the DOB field on the page
+			var cat_dob = cat_dob_divs[i].innerHTML;
+			var cat_dob_details = cat_dob.split("/");
+			
+			var month = parseInt(cat_dob_details[0]) - 1;
+			var day = parseInt(cat_dob_details[1]);
+			var year = parseInt(cat_dob_details[2]) + 2000;
 
-		// create js and moment date objects
-		var jsdob = new Date(year, month, day);
-		var momentdob = moment(jsdob);
+			// create js and moment date objects
+			var jsdob = new Date(year, month, day);
+			var momentdob = moment(jsdob);
 
-		// get string and set it on thee page
-		var ageString = momentdob.fromNow(1);
-		cat_age_divs[i].innerHTML = ageString;
-	}
+			// get string and set it on the page
+			var ageString = momentdob.fromNow(1);
+			cat_age_divs[i].innerHTML = ageString;
+		}
+	});
 }
 
 $(function () {
-
-})
+	
+});
