30815c28c77a1f645ece93dc0c260f6f42a79956
Robert Rico <robert.m.rico@gmail.com>
2017-03-18 13:23:05 -0700

    Testing Litter FIlter

--- TestingAssets/Litters/litter_filter.py
+++ TestingAssets/Litters/litter_filter.py
@@ -5,6 +5,7 @@ import random
 import string
 import re
 
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import selenium.webdriver.chrome.service as service
@@ -19,7 +20,6 @@ try:
 
 
 	db.query('INSERT INTO litters(kc_ref_id, litter_name, the_cat_count, kitten_count, dob, asn_start, asn_end, est_arrival, breed, foster_notes, notes, created, is_deleted) VALUES('+rand_KC+', "'+rand_name1+'", 0, 0, "2017-02-14", "2017-03-14", "2017-03-21", "Early/Mid March", "Calico minx tabby mixes", "This litter is no longer coming in, hence why it\'s deleted", "Other litter notes... needs etc.", NOW(),false);');
-
 	db.store_result()
 
 	db.query('SELECT id FROM litters where kc_ref_id="'+rand_KC+'" AND litter_name="'+rand_name1+'";')
@@ -27,7 +27,7 @@ try:
 	r=db.store_result()
 
 	k=r.fetch_row(1,1)
-	litter_id = k[0].get('id')
+	cat_id = k[0].get('id')
 
 
 	service = service.Service('D:\ChromeDriver\chromedriver')
@@ -38,18 +38,19 @@ try:
 
 	driver.set_window_size(sys.argv[1], sys.argv[2]);
 
-	driver.get('http://localhost:8765/litters/view/'+litter_id);
+	driver.get('http://localhost:8765/litters/index')
 	
-	l_name_elem = driver.find_element_by_id("litterName")
-	l_name= l_name_elem.text
-
-	if l_name == rand_name1:
+	elem = driver.find_element_by_id("litterNameFilter")
+	elem.send_keys(rand_name1)
+	
+	driver.find_element_by_id("litterFilter").click()
+	
+	a = driver.find_element_by_css_selector('a[href="/litters/view/'+cat_id+'"].card > div > div.card-h2')
+	
+	if rand_name1 == a.text:
 		print("pass")
 	else:
 		print("fail")
-
-	driver.quit()
 except Exception as e:
 	print(e)
 	print("fail")
-
