54498494f4c7dcf791ab1c163278c1fa3bceaa1a
Robert Rico <robert.m.rico@gmail.com>
2017-03-18 14:25:27 -0700

    Foreign Constraint Fails if cats exist, set breed_id to 1

--- DB/sprint3_deploy.sql
+++ DB/sprint3_deploy.sql
@@ -115,7 +115,9 @@ END IF;
 IF NOT EXISTS ((SELECT * FROM information_schema.columns WHERE table_schema=DATABASE() AND table_name='cats' AND column_name='breed_id'))
 THEN
 ALTER TABLE cats ADD breed_id INT NOT NULL;
+UPDATE cats SET breed_id=1;
 ALTER TABLE cats ADD CONSTRAINT breed_ref FOREIGN KEY (breed_id) REFERENCES breeds(id);
+
 END IF;
 
 
