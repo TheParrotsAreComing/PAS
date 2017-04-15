4c3504a6b05f764b0873eb14160a34a5a1f7af6f
Ron Lewis <ron.lewis@gmail.com>
2017-04-15 11:54:33 -0700

    fix bad merge

--- src/Model/Table/AdoptersTable.php
+++ src/Model/Table/AdoptersTable.php
@@ -41,6 +41,7 @@ class AdoptersTable extends Table
         $this->primaryKey('id');
 
         $this->addBehavior('Timestamp');
+        $this->addBehavior('File');
         $this->addBehavior('FilterableTag');
 
         $this->hasMany('CatHistories', [
