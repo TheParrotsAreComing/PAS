7dc04cf1b3c38481bb3c526d0f90b439499098aa
Ron Lewis <ron.lewis@gmail.com>
2017-04-28 19:39:33 -0700

    lotsa stuff to get photos working with volunteers

--- webroot/js/global.js
+++ webroot/js/global.js
@@ -1,5 +1,23 @@
 var APP_PATH = window.location.origin+"/";
 
+jQuery(function () {
+    //Show me the new select option for photos
+    $('a[data-ix="add-photo-click-desktop"]').click(function(){
+        $('.add-photo').css('display','flex');
+        $('.add-photo').css('opacity','1');
+        $('.add-photo-inner').css('display','flex');
+        $('.add-photo-inner').css('opacity','1');
+    });
+
+    //I don't want to see the select for photos
+    $('.cancel').click(function(){
+        $('.add-photo').css('display','none');
+        $('.add-photo-inner').css('display','none');
+        $('.add-photo-inner').css('opacity','0');
+    });
+});
+
+
 function setupPhotoSelectionBehavior(cat_id) {
 
     $('.picture-file').click(function(){
