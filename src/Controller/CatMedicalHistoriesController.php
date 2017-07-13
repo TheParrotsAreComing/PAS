7f8f63d2186757c1bc0fb5f935a7c474bd99e7ba
Eric Bollinger <ericsbollinger@gmail.com>
2017-07-13 01:27:07 -0700

    Download file, not display.
    Signed-off-by: Eric Bollinger <ericsbollinger@gmail.com>

--- src/Controller/CatMedicalHistoriesController.php
+++ src/Controller/CatMedicalHistoriesController.php
@@ -248,7 +248,7 @@ class CatMedicalHistoriesController extends AppController
 
         $this->loadComponent('MedicalHistory');
         $this->MedicalHistory->drawDoc($pdf, $histories);
-        $pdf->Output($histories['cat']['cat_name'].' Medical History', 'I');
+        $pdf->Output($histories['cat']['cat_name'].' Medical History.pdf', 'D');
 
 
     }
