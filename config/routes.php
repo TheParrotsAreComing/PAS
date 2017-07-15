b89bc34868bdddca091a9e7bb69cb29092f00ebd
Robert Rico <robert.m.rico@gmail.com>
2017-07-14 19:09:46 -0700

    Route Fix

--- config/routes.php
+++ config/routes.php
@@ -56,6 +56,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      * ...and connect the rest of 'Pages' controller's URLs.
      */
     $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
+    $routes->connect('/files',['controller' => 'files', 'action' => 'index']);
 
     /**
      * Connect catchall routes for all controllers.
